6227f509424005530cea407c5b6a5fa9e75174e0
NoeSourdes <sourdesnoe19@gmail.com>
2024-02-20 01:50:28 +0100

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Providers } from "./providers/providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +16,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html className="dark" lang="fr">
+      <body className={inter.className}>
+        <main className="text-foreground bg-background">
+          <Providers>{children}</Providers>
+        </main>
+      </body>
     </html>
   );
 }
